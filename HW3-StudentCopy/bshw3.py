47902359c54d5b554c8746aeea5d1a3eb3ce8cce
David Kim <dkeem@umich.edu>
2016-11-11 16:28:16 -0500

    Updated

--- HW3-StudentCopy/bshw3.py
+++ HW3-StudentCopy/bshw3.py
@@ -14,7 +14,7 @@ webPage = soup.prettify()
 #Replaces everything that needed to be replaced
 webPage = webPage.replace("student", "AMAZING student")
 webPage = webPage.replace("students", "AMAZING students")
-webPage = webPage.replace("https://testbed.files.wordpress.com/2012/09/bsi_exposition_041316_192.jpg", "media\pic.jpg")
+webPage = webPage.replace("https://testbed.files.wordpress.com/2012/09/bsi_exposition_041316_192.jpg", "https://scontent.fdet1-1.fna.fbcdn.net/v/t1.0-0/p206x206/14141962_10153874492878359_3419118872454018419_n.jpg?oh=48b3ae049106348e65a16dfed3075466&oe=58961321")
 webPage = webPage.replace("logo2.png", "media\logo.png")
 
 #This writes the html file that was created above with the webpage and the changes made
